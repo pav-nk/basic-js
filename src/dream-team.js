5704c31333c07a2007abab3bf761dafd73cc382e
pav_nk <pav_nk@vk.com>
2024-12-24 15:54:19 +0300

    feat: dream-team task is completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const normalizedMembers = members.filter((item) => typeof item === 'string').map((item) => item.toLowerCase().trim());
+  return normalizedMembers.map((name) => name[0].toUpperCase()).sort().join('');
 }
 
 module.exports = {
