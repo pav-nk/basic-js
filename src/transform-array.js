dea7702ae8e25930b42360d415aec28043942fc7
pav_nk <pav_nk@vk.com>
2024-12-27 12:10:12 +0300

    feat: transform-array task is completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) throw new Error(`'arr' parameter must be an instance of the Array!`);
+
+  let arrCopy = [...arr];
+
+  const actionMap = {
+    "--discard-next": {step: 1, value: 'delete'},
+    "--discard-prev": {step: -1, value: 'delete'},
+    "--double-next": {step: 1, value: 'add'},
+    "--double-prev": {step: -1, value: 'add'},
+  }
+
+  for (let i = 0; i < arrCopy.length; i += 1) {
+      if (actionMap.hasOwnProperty(arrCopy[i])) {
+        const action = actionMap[arrCopy[i]];
+        const newIdx = i + action.step;
+        if (!arrCopy[newIdx]) continue;
+        if (action.value === 'delete') {
+          arrCopy.splice(newIdx, 1);
+        } else {
+          arrCopy.splice(i, 1, arrCopy[i + action.step]);
+        }
+      }
+  }
+  return arrCopy.filter((el) => !actionMap.hasOwnProperty(el));
 }
 
 module.exports = {
